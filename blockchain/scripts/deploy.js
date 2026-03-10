ef2fb408e1f2c79e519904424419ae685e696b17
Tousif Ansari <tousifansari75676@gmail.com>
2026-03-10 12:40:17 +0530

    added the deploy.js in script and compile

---
+++ blockchain/scripts/deploy.js
@@ -0,0 +1,24 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  const Escrow = await hre.ethers.getContractFactory("Escrow");
+
+  const freelancerAddress = "0x0000000000000000000000000000000000000001";
+
+  const escrow = await Escrow.deploy(freelancerAddress, {
+    value: hre.ethers.utils.parseEther("1")
+  });
+
+  await escrow.deployed();
+
+  console.log("Escrow deployed to:", escrow.address);
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
